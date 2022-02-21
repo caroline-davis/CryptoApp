881c5314a1eb0b5c7a02effb483285911c6e13b4
Caroline Davis <caroline.davis@digio.com.au>
2022-02-21 15:49:37 +1100

    feat: Add coin row view file

---
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  SwiftfulCrypto
+//
+//  Created by Caroline Davis on 21/2/22.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: CoinModel)
+    }
+}
