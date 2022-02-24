a7b040fb6904478102be75f181aa769100bacf47
Caroline Davis <caroline.davis@digio.com.au>
2022-02-24 14:40:57 +1100

    feat: Add statistic view

---
+++ SwiftfulCrypto/Extensions/Models/StatisticsModel.swift
@@ -0,0 +1,23 @@
+//
+//  StatisticsModel.swift
+//  SwiftfulCrypto
+//
+//  Created by Caroline Davis on 24/2/22.
+//
+
+import Foundation
+
+struct StatisticModel: Identifiable {
+
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+
+}
